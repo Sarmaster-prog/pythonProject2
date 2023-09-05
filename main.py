85126fe8f4d8c0d7b05f1fdc3657ec7df5b6cf37
User <srozkov63@gmail.com>
2023-09-05 22:26:14 +0300

    fgggff

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ capitalize("how are you!") # [How Are You!]
 n=5
 string="Hello World "
 print(string * n)
+
+def merge(dic1,dic2):
+    dic3=dic1.copy()
+    dic3.update(dic2)
+    return dic3
+dic1={1:"hello", 2:"world"}
+dic2={3:"Python", 4:"Programming"}
+merge(dic1,dic2) # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'}
