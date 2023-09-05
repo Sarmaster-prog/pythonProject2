79c9b055498d5b4c9de139ea2d9b6cae157a061f
User <srozkov63@gmail.com>
2023-09-05 22:36:36 +0300

    сссс

--- main.py
+++ main.py
@@ -21,3 +21,14 @@ def merge(dic1,dic2):
 dic1={1:"hello", 2:"world"}
 dic2={3:"Python", 4:"Programming"}
 merge(dic1,dic2) # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'}
+
+import time
+start_time= time.time()
+def fun():
+    a=2
+    b=3
+    c=a+b
+end_time= time.time()
+fun()
+timetaken = end_time - start_time
+print("Your program takes: ", timetaken) # 0.0345
